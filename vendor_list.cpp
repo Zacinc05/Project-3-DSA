a1db025583a0c0d73f612d9e17a613ac55b4e351
Zacinc05 <166748475+Zacinc05@users.noreply.github.com>
2025-04-18 20:41:30 -0400

    update smp

--- vendor_list.cpp
+++ vendor_list.cpp
@@ -13,16 +13,64 @@ using namespace std;
 // Market price of each item.
 unordered_map<string, double> standard_market_price() {
 
-    unordered_map<string, double> smp;
-
-    smp["Charizard"] = 120;
-    smp["Pikachu"] = 18;
-    smp["Bulbasaur"] = 8;
-    smp["Squirtle"] = 11;
-    smp["Jigglypuff"] = 8;
-    smp["Eevee"] = 12;
-    smp["Meowth"] = 8;
-    smp["Rizz"] = 8;
+    unordered_map<string, double> smp = {
+            {"Bulbasaur", 75.00}, {"Ivysaur", 150.00}, {"Venusaur", 300.00},
+            {"Charmander", 100.00}, {"Charmeleon", 200.00}, {"Charizard", 3000.00},
+            {"Squirtle", 80.00}, {"Wartortle", 175.00}, {"Blastoise", 400.00},
+            {"Caterpie", 15.00}, {"Metapod", 20.00}, {"Butterfree", 30.00},
+            {"Weedle", 12.00}, {"Kakuna", 18.00}, {"Beedrill", 25.00},
+            {"Pidgey", 20.00}, {"Pidgeotto", 30.00}, {"Pidgeot", 50.00},
+            {"Rattata", 10.00}, {"Raticate", 15.00}, {"Spearow", 12.00},
+            {"Fearow", 25.00}, {"Ekans", 15.00}, {"Arbok", 25.00},
+            {"Pikachu", 300.00}, {"Raichu", 40.00}, {"Sandshrew", 20.00},
+            {"Sandslash", 35.00}, {"Nidoran♀", 20.00}, {"Nidorina", 30.00},
+            {"Nidoqueen", 60.00}, {"Nidoran♂", 20.00}, {"Nidorino", 30.00},
+            {"Nidoking", 60.00}, {"Clefairy", 30.00}, {"Clefable", 50.00},
+            {"Vulpix", 40.00}, {"Ninetales", 75.00}, {"Jigglypuff", 25.00},
+            {"Wigglytuff", 35.00}, {"Zubat", 10.00}, {"Golbat", 20.00},
+            {"Oddish", 20.00}, {"Gloom", 30.00}, {"Vileplume", 40.00},
+            {"Paras", 12.00}, {"Parasect", 18.00}, {"Venonat", 15.00},
+            {"Venomoth", 25.00}, {"Diglett", 15.00}, {"Dugtrio", 25.00},
+            {"Meowth", 30.00}, {"Persian", 40.00}, {"Psyduck", 20.00},
+            {"Golduck", 30.00}, {"Mankey", 12.00}, {"Primeape", 25.00},
+            {"Growlithe", 50.00}, {"Arcanine", 120.00}, {"Poliwag", 20.00},
+            {"Poliwhirl", 30.00}, {"Poliwrath", 40.00}, {"Abra", 25.00},
+            {"Kadabra", 35.00}, {"Alakazam", 70.00}, {"Machop", 15.00},
+            {"Machoke", 25.00}, {"Machamp", 40.00}, {"Bellsprout", 15.00},
+            {"Weepinbell", 20.00}, {"Victreebel", 30.00}, {"Tentacool", 10.00},
+            {"Tentacruel", 20.00}, {"Geodude", 15.00}, {"Graveler", 25.00},
+            {"Golem", 35.00}, {"Ponyta", 30.00}, {"Rapidash", 45.00},
+            {"Slowpoke", 20.00}, {"Slowbro", 30.00}, {"Magnemite", 25.00},
+            {"Magneton", 40.00}, {"Farfetch'd", 30.00}, {"Doduo", 15.00},
+            {"Dodrio", 25.00}, {"Seel", 15.00}, {"Dewgong", 25.00},
+            {"Grimer", 12.00}, {"Muk", 20.00}, {"Shellder", 10.00},
+            {"Cloyster", 30.00}, {"Gastly", 20.00}, {"Haunter", 30.00},
+            {"Gengar", 250.00},
+            {"Onix", 35.00}, {"Drowzee", 18.00}, {"Hypno", 28.00},
+            {"Krabby", 10.00}, {"Kingler", 20.00}, {"Voltorb", 15.00},
+            {"Electrode", 25.00}, {"Exeggcute", 20.00}, {"Exeggutor", 30.00},
+            {"Cubone", 25.00}, {"Marowak", 35.00}, {"Hitmonlee", 40.00},
+            {"Hitmonchan", 40.00}, {"Lickitung", 30.00}, {"Koffing", 15.00},
+            {"Weezing", 25.00}, {"Rhyhorn", 30.00}, {"Rhydon", 45.00},
+            {"Chansey", 100.00},
+            {"Tangela", 20.00}, {"Kangaskhan", 40.00}, {"Horsea", 10.00},
+            {"Seadra", 20.00}, {"Goldeen", 10.00}, {"Seaking", 15.00},
+            {"Staryu", 20.00}, {"Starmie", 35.00}, {"Mr. Mime", 30.00},
+            {"Scyther", 60.00},
+            {"Jynx", 20.00}, {"Electabuzz", 40.00}, {"Magmar", 35.00},
+            {"Pinsir", 25.00}, {"Tauros", 30.00}, {"Magikarp", 5.00},
+            {"Gyarados", 150.00},
+            {"Lapras", 80.00},
+            {"Ditto", 25.00}, {"Eevee", 100.00},
+            {"Vaporeon", 30.00}, {"Jolteon", 30.00}, {"Flareon", 30.00},
+            {"Porygon", 30.00}, {"Omanyte", 20.00}, {"Omastar", 30.00},
+            {"Kabuto", 20.00}, {"Kabutops", 30.00}, {"Aerodactyl", 50.00},
+            {"Snorlax", 50.00}, {"Articuno", 90.00}, {"Zapdos", 90.00},
+            {"Moltres", 90.00}, {"Dratini", 30.00}, {"Dragonair", 60.00},
+            {"Dragonite", 250.00},
+            {"Mewtwo", 300.00},
+            {"Mew", 200.00}
+            };
 
     return smp;
 }
